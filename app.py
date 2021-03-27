61aa7207d33b552bc0ba1df4bad5f5f828126f49
narender <narender.nain@siemens.com>
2021-03-27 16:06:35 +0530

    edit in app

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask , render_template, request ,  make_response , redirect , url_for
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+#from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
 import cv2
 from PIL import Image
 import numpy
